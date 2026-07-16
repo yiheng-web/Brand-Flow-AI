415a5ccce1c64f88d210a9f1af2419fa098e644f
zuoyu <zuoyu@dtstack.com>
2026-07-16 14:51:07 +0800

    test

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { router } from './router'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
 )
+//test
